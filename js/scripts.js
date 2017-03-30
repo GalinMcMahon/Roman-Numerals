a1a3aec7c59277190f212e126accfdb89c756eab
Galin McMahon and Mohamed Warsame <mohamed@>
2017-03-30 14:23:57 -0700

    add logic for all numbers between 1-99.

--- js/scripts.js
+++ js/scripts.js
@@ -6,9 +6,10 @@ var C = 100;
 var D = 500;
 var M = 1000;
 var output = [];
-var str = "I"
-
-
+var str1 = "I"
+var str2 = "X"
+var str3 = "C"
+var str4 = "M"
 
 $(document).ready(function() {
   $("form#roman-numeral").submit(function(event) {
@@ -18,26 +19,76 @@ $(document).ready(function() {
   //     alert("Please enter an integer less than 4000")
   //
   // }
+    // ONE POSITION
       var onePosition = (userInput.slice(-1));
       if (onePosition < 4 && onePosition > 0) {
-        var ones = str.repeat(onePosition);
-        alert(ones)
+        var ones = str1.repeat(onePosition);
+        // alert(ones)
 
       } else if
-        (onePosition > 4 && onePosition < 9) {
-          var fiveeight = (onePosition-5);
-          var fives = str.repeat(fiveeight);
-          alert("V"+fives)
+          (onePosition > 4 && onePosition < 9) {
+            var fiveeight = (onePosition-5);
+            var fives = str1.repeat(fiveeight);
+            var ones = ("V"+fives)
 
       } else if (onePosition == 4) {
-          alert("IV")
+          var ones = ("IV")
 
       } else if (onePosition == 9) {
-          alert("IX")
-      } else {
-        alert("Please enter 1-9")
+          var ones = ("IX")
+      } else if (onePosition == 0) {
+        var ones = ("")
       }
+      // } else {
+      //   alert("")
+      // }
 
+      if (userInput === onePosition) {
+        alert (ones)
+      } else
+
+      // TEN POSITION
+      var twoPosition = (userInput.slice(-2, 1));
+      if (twoPosition < 4 && twoPosition > 0) {
+        var twos = str2.repeat(twoPosition);
+        alert(twos+ones)
+
+    } else if
+        (twoPosition > 4 && twoPosition < 9) {
+          var fiveeight = (twoPosition-5);
+          var fives = str2.repeat(fiveeight);
+          var twos = ("L"+fives)
+          alert(twos+ones)
+    } else if (twoPosition == 4) {
+        var twos = ("XL")
+        alert(twos+ones)
+    } else if (twoPosition == 9) {
+        var twos = ("XC")
+        alert(twos+ones)
+    } else {
+      alert("Please enter 1-9")
+    }
+
+    if (userInput === twoPosition) {
+      alert (twos+ones)
+    } else
+
+
+
+
+
+
+
+
+
+
+
+      // var numberLength = (userInput.length);
+      // if (numberLength == 1) {
+      //   alert("user input is 1 digit long")
+      // } else if (numberLength == 2) {
+      //   alert("user input is 2 digits long")
+      // }
 
 
     event.preventDefault();
